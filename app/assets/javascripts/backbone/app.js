a6ff7349aa2fe8cb63d25fb92d2fab9d4ac0516a
mattdvhope <mattagape@me.com>
2018-04-10 15:11:51 -0400

    Removed unnecessary id's

--- app/assets/javascripts/backbone/app.js
+++ app/assets/javascripts/backbone/app.js
@@ -6,8 +6,6 @@
 //= require_tree ./routers
 //= require_tree ./app_methods
 
-console.log("Hello!!!!");
-
 var App = {
   getFrontMainPage: function() {
     getFrontMainPage(); // in 'app_methods' folder
